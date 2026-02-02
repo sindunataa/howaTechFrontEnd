3dffe7eefd01613dbfdc166a43702c624fa4de71
sindunata <sindunata61@gmail.com>
2026-02-03 00:49:25 +0700

    update

--- src/stores/api.js
+++ src/stores/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/v1',
   timeout: 10000,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -5,28 +5,14 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/', 
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
   },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000', // Laravel backend
-        changeOrigin: true,
-      }
-    }
-  },
   build: {
-    outDir: '../company-profile-backend/public/build', // ⬅️ arahkan hasil build ke Laravel
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: {
-        app: resolve(__dirname, 'index.html'), // ⬅️ penting untuk Laravel load HTML + JS
-      }
-    }
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
